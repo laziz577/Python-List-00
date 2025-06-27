7b13a3a338eb0703393b36018cf25c3dd161a9b4
laziz577 <temirovabdulaziz042@gmail.com>
2025-06-27 11:59:18 +0500

    bajarildi

---
+++ task21.py
@@ -0,0 +1,7 @@
+ruyxat = ['Andijon','Buxoro','Namangan','Toshkent']
+delet = input("Qaysi shaxarni uchirasiz:")
+
+if delet in ruyxat:
+    ruyxat.remove(delet)
+    print("yangi ruyxat",ruyxat)
+    
