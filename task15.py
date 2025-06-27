3d20faea91cdd71cb463ed1a379c83965f7d4781
laziz577 <temirovabdulaziz042@gmail.com>
2025-06-27 11:34:43 +0500

    bajarildi

---
+++ task15.py
@@ -0,0 +1,7 @@
+raqamlar = [12,22,32,42,54,55,65,45]
+start = int(input("(start) ni kiriting:"))
+end = int(input("(end) ni kiriting:"))
+
+result = raqamlar[start:end]
+
+print(f"{start} dan {end} gacha:",result)
